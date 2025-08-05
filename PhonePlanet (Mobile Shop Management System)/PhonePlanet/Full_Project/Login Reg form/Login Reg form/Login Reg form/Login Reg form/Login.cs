0dc8272da732a44fbd4c169b949ab98ae786c14b
MD. ABDUL AZIZ TAWSIF <aatawsif@gmail.com>
2025-08-05 15:03:47 +0600

    Using C# , Microsoft SQL

---
+++ PhonePlanet (Mobile Shop Management System)/PhonePlanet/Full_Project/Login Reg form/Login Reg form/Login Reg form/Login Reg form/Login.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Login_Reg_form
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=TAWSIF-AZIZ-AHM\\SQLEXPRESS;Initial Catalog=PhonePlanetDB;Integrated Security=True");
+
+        private void Log_label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            Start start = new Start();
+            start.Show();
+            this.Hide();
+        }
+
+        private void Forgotpass_Click(object sender, EventArgs e)
+        {
+            Varify_Form vf = new Varify_Form();
+            vf.Show();
+            this.Hide();
+        }
+
+        private void create_new_acc_label_Click(object sender, EventArgs e)
+        {
+            Registration registration = new Registration();
+            registration.Show();
+            this.Hide();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Signin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CustomerSignin_Click(object sender, EventArgs e)
+        {
+            string query = "select * from Approved_Customer where User_Name='" + tb_username.Text + "'and Password='" + tb_password.Text + "'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                MessageBox.Show("Customer Login Successful!!");
+            }
+            else
+            {
+                MessageBox.Show("Wrong Credentials!!\n Try Again");
+                tb_username.Clear();
+                tb_password.Clear();
+            }
+        }
+
+        private void Admin_Signin_Click(object sender, EventArgs e)
+        {
+            string query = "select * from Admin_Login where User_Name='" + tb_username.Text + "'and Password='" + tb_password.Text + "'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                MessageBox.Show("Admin Login Successful!!");
+            }
+            else
+            {
+                MessageBox.Show("Wrong Credentials!!\n Try Again");
+                tb_username.Clear();
+                tb_password.Clear();
+            }
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PhonePlanet (Mobile Shop Management System)/PhonePlanet/Full_Project/Login Reg form/Login Reg form/Login Reg form/Login Reg form/Registration.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Login_Reg_form
+{
+    public partial class Registration : Form
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=AYAN\\SQLEXPRESS;Initial Catalog=PhonePlanetDB;Integrated Security=True");
+
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            Start start = new Start();
+            start.Show();
+            this.Hide();
+        }
+
+        private void Signup_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Already_acc_Click_1(object sender, EventArgs e)
+        {
+
+            Login login = new Login();
+            login.Show();
+            this.Hide();
+        }
+
+
+
+        private void Exit_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void CustomerSignup_Click(object sender, EventArgs e)
+        {
+            /*Login login = new Login();
+            login.Show();
+            this.Hide();*/
+
+
+            {
+
+                string query = "insert into Registration values('" + tb_name.Text + "','" + tb_username.Text + "','" + tb_mobileno.Text + "','" + tb_email.Text + "','" + tb_address.Text + "','" + tb_password.Text + "')";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                con.Open();
+
+                if (con.State == ConnectionState.Open)
+                {
+                    int result = cmd.ExecuteNonQuery();
+
+                    if (result > 0)
+                        MessageBox.Show("Registration Successful!!! Wait For Admin Approval");
+                }
+                con.Close();
+
+                Start s = new Start();
+                s.Show();
+                this.Hide();
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "insert into Admin_Login values('" + tb_name.Text + "','" + tb_username.Text + "','" + tb_mobileno.Text + "','" + tb_email.Text + "','" + tb_address.Text + "','" + tb_password.Text + "')";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+
+            if (con.State == ConnectionState.Open)
+            {
+                int result = cmd.ExecuteNonQuery();
+
+                if (result > 0)
+                    MessageBox.Show(" Admin Registration Successful!!! \n You May Login");
+            }
+            con.Close();
+
+            Start s = new Start();
+            s.Show();
+            this.Hide();
+        }
+
+        private void tb_password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+  
